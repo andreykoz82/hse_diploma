1f45ae33bed86b9c847a702386c8a18778ec5fdc
Andrey <47432367+andreykoz82@users.noreply.github.com>
2021-11-11 10:35:56 +0300

    Create mygen.py

---
+++ synthtext/mygen.py
@@ -0,0 +1,141 @@
+# %%
+from itertools import chain
+
+import random
+import string
+import os
+import os.path as osp
+
+from PIL import Image
+
+import h5py
+import pickle as cp
+
+from synthgen import *
+from common import *
+
+# %%
+label_map = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12,
+             'D': 13,
+             'E': 14, 'F': 15, 'G': 16, 'H': 17, 'I': 18, 'J': 19, 'K': 20, 'L': 21, 'M': 22, 'N': 23, 'O': 24, 'P': 25,
+             'Q': 26, 'R': 27,
+             'S': 28, 'T': 29, 'U': 30, 'V': 31, 'W': 32, 'X': 33, 'Y': 34, 'Z': 35}
+
+
+def sn_generator(size=13, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def gtin_generator(size=5, chars=string.digits):
+    return '046014980' + ''.join(random.choice(chars) for _ in range(size))
+
+
+def batch_generator():
+    date = ''.join(str(random.randint(1, 31)))
+    if len(date) == 1:
+        date = '0' + date
+
+    month = ''.join(str(random.randint(1, 12)))
+    if len(month) == 1:
+        month = '0' + month
+    year = ''.join(str(random.randint(20, 25)))
+    return date + month + year
+
+
+def exp_generator():
+    month = ''.join(str(random.randint(0, 12)))
+    if len(month) == 1:
+        month = '0' + month
+    year = ''.join(str(random.randint(2020, 2025)))
+    return month + ' ' + year
+
+
+def gen(N=5000):
+    for i in range(N):
+        gtin = gtin_generator()
+        sn = sn_generator()
+        batch = batch_generator()
+        exp = exp_generator()
+        yield gtin, sn, batch, exp
+
+
+with open('data/newsgroup/newsgroup.txt', 'w') as f:
+    for box in gen():
+        for label in box:
+            f.write(label + '\n')
+# %%
+
+INSTANCE_PER_IMAGE = 20
+
+im_dir = 'bg_img'
+depth_db = h5py.File('depth.h5', 'r')
+seg_db = h5py.File('seg.h5', 'r')
+
+imnames = sorted(depth_db.keys())
+
+RV3 = RendererV3('data', max_time=5)
+
+
+def to_yolo(imgname, res, root_img, root_meta):
+
+    for i, item in enumerate(res):
+        Image.fromarray(item['img']).save(osp.join(root_img, f'{imgname}-{i}.png'))
+        imh, imw = item['img'].shape[:2]
+
+        chars = list(filter(lambda x: x in string.ascii_uppercase + string.digits,
+                            chain(*item['txt'])))
+
+        assert len(chars) == item['charBB'].shape[-1]
+
+        with open(osp.join(root_meta, f'{imgname}-{i}.txt'), 'w') as f:
+            for char, charBB in zip(chars, item['charBB'].transpose(2, 0, 1)):
+                x_min, y_min = charBB.min(axis=1)
+                x_max, y_max = charBB.max(axis=1)
+                x_center = .5 * (x_min + x_max) / imw
+                y_center = .5 * (y_min + y_max) / imh
+                width = (x_max - x_min) / imw
+                height = (y_max - y_min) / imh
+                f.write(f'{label_map[char]} {x_center} {y_center} {width} {height}\n')
+
+
+for imname in sorted(os.listdir(im_dir)):
+    if imname < 'hubble_4':
+        continue
+    print(f'start {imname} ...')
+    if not any(imname.endswith(ext) for ext in ('.jpg', '.jpeg', '.png')):
+        continue
+
+    try:
+        img = Image.open(osp.join(im_dir, imname)).convert('RGB')
+    except:
+        print(f'bad img {imname}')
+        continue
+    try:
+        depth = depth_db[imname][:].T
+    except:
+        print(f'no depth for {imname}')
+        continue
+    depth = depth[:, :, 0]
+
+    try:
+        seg = seg_db['mask'][imname][:].astype('float32')
+        area = seg_db['mask'][imname].attrs['area']
+        label = seg_db['mask'][imname].attrs['label']
+    except:
+        print(f'no seg for {imname}')
+        continue
+
+    sz = depth.shape[:2][::-1]
+    img = np.array(img.resize(sz, Image.ANTIALIAS))
+    seg = np.array(Image.fromarray(seg).resize(sz, Image.NEAREST)).astype('float32')
+
+    try:
+        res = RV3.render_text(img, depth, seg, area, label,
+                              ninstance=INSTANCE_PER_IMAGE)
+    except:
+        print(f'{imname} problems while text rendering')
+    if len(res) > 0:
+        to_yolo(imname, res, 'results/images',
+                'results/labels')
+
+    print(f'end {imname}')
